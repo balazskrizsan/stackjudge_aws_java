d4ea43c13623b5c4abb1aa69960b26c802eb0447
Balazs Krizsan <krizsan.balazs@gmail.com>
2023-09-30 20:49:22 +0200

    JOOQ generator added

---
+++ jooq/gen.bat
@@ -0,0 +1,9 @@
+"W:\Java\jdk-17.0.1\bin\java.exe" -cp ^
+jooq-3.17.7.jar;^
+jooq-meta-3.17.7.jar;^
+jooq-codegen-3.17.7.jar;^
+reactive-streams-1.0.3.jar;^
+jakarta.xml.bind-api-3.0.0.jar;^
+r2dbc-spi-0.9.1.RELEASE.jar;^
+postgresql-42.2.12.jar ^
+org.jooq.codegen.GenerationTool ./gen-config.xml
